4ae2672b82542441a4b25feea830adc6a7ba66ff
SOONYOUNG <sowkoung@gmail.com>
2026-05-22 18:34:42 +0900

    MyLocationSet Map

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,14 @@ import { NavLink } from "react-router-dom"
 import LoginHeader from "../features/member/components/LoginHeader";
 
 export default function Header(){
+  const memberIsLogin = sessionStorage.getItem("memberIsLogin");
+	const member = memberIsLogin ? JSON.parse(memberIsLogin) : null;
+  const isLogin = member !== null;
+
   return (
     <header>
       <div><NavLink to='/'>홈</NavLink></div>
-      <div><NavLink to='/myLocationSet'>내 위치 설정</NavLink></div>
+      {isLogin ? <div><NavLink to='/myLocationSet'>내 위치 설정</NavLink></div> : <div><NavLink to='/login'>내 위치 설정</NavLink></div> }
       <div><NavLink to='/'>검색창</NavLink></div>
       <LoginHeader />
       
