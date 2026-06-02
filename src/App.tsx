e24b741f8bf81285488e5956204c7c8b966ea718
taemin63 <giltaemin0603@gmail.com>
2026-06-02 15:03:52 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,19 @@
-<<<<<<< HEAD
-// src/App.tsx
-import './App.scss';
-import TopBanner from './components/TopBanner';
-import Header from './components/Header';
-import Section from './components/Section';
-import Footer from './components/Footer';
-import { FilterSearchProvider } from './features/filter/context/FilterSearchContext';
+import '@/App.scss';
+import TopBanner from '@/components/TopBanner';
+import Header from '@/components/Header';
+import Section from '@/components/Section';
+import Footer from '@/components/Footer';
+import { FilterSearchProvider } from '@/features/filter/context/FilterSearchContext';
 
 export default function App() {
   return (
     <FilterSearchProvider>
-      <div className="app-container">
+      <div className="appContainer">
         <TopBanner />
         <Header />
         <Section />
         <Footer />
       </div>
     </FilterSearchProvider>
-=======
-import TopBanner from './components/TopBanner'
-import Header from './components/Header'
-import Section from './components/Section'
-import Footer from './components/Footer'
-import "@/App.scss";
-
-export default function App() {
-  return (
-    <>
-    <div className="appContainer">
-      <TopBanner />
-      <Header />
-      <Section />
-      <Footer />
-    </div>
-    </>
->>>>>>> bad76a465ef2fe50cbd3a193dd761eaf6e4f6a3c
   );
 }
