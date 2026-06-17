26ef71bc5b5ed5e4c4aa351af7aa4469e7e3d2b2
SOONYOUNG <sowkoung@gmail.com>
2026-06-17 18:04:21 +0900

    change MyLocation Modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import LoginHeader from "../features/member/components/LoginHeader";
 import useCustomLogin from "@/hooks/useCustomLogin";
 import KeywordSearchBar from "@/features/filter/components/KeywordSearchBar";
@@ -6,15 +6,26 @@ import styles from "@/components/_header.module.scss";
 
 export default function Header(){
   const { isLogin } = useCustomLogin();
-
+  const location = useLocation();
+  
   return (
     <header className={styles.header}>
       <NavLink to="/" className={styles.logoLink}>
         <img src="/logo_1.png" alt="오늘의 식당 로고" />
   	  </NavLink>
-      {isLogin ? <div><NavLink to='/my-location-set'>내 위치 설정</NavLink></div> 
-               : <div><NavLink to='/member/login'>내 위치 설정</NavLink></div> }
 
+      {isLogin ? (
+        <div>
+          <NavLink to='/my-location-set' state={{ backgroundLocation: location }}>
+            내 위치 설정
+          </NavLink>
+        </div> 
+      ) : (
+        <div>
+          <NavLink to='/member/login'>내 위치 설정</NavLink>
+        </div>
+      )}
+      
       <div id="food-type-slot">
         <KeywordSearchBar />
       </div>
