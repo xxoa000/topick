0d23fbcf04a21ec3c7f22a43ceec04643f89e035
다이다이 <xxoa000@gmail.com>
2026-06-25 09:30:23 +0900

    마이페이지-내정보 수정 기능 추가

--- src/features/myInfo/pages/MyUpdatePage.tsx
+++ src/features/myInfo/pages/MyUpdatePage.tsx
@@ -1,16 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import s from "@/features/myInfo/pages/_my-update-page.module.scss";
 import type { JoinFormDTO } from "@/features/member/types/joinDTO";
+import memberApi from "@/features/member/services/memberApi";
+import useCustomLogin from "@/hooks/useCustomLogin";
+import axios from "axios";
 
 export default function MyUpdatePage() {
   const [emailDomain, setEmailDomain] = useState("");
+  const { member } = useCustomLogin();
 
+  // reset() : 폼의 값을 한번에 초기화 하거나 덮어쓰는 함수, 내 정보 수정용
   const {
     register,
+    handleSubmit,
     setValue,
+    reset,
+    setError,
     formState: { errors },
   } = useForm<JoinFormDTO>();
+
+  // 화면 출력용
+  useEffect(()=>{
+    const getInfo = async() => {
+      if (!member?.memberId) return; //에러방지 코드
+
+      const data = await memberApi.selectOne(member?.memberId);
+      const [emailFirst, emailLast] = data.memberEmail?.split("@") ?? ["",""];
+      reset({
+        memberId: data.memberId,
+        memberName: data.memberName,
+        emailFirst,
+        emailLast,
+        memberPhone: data.memberPhone ?? null,
+        memberGender: data.memberGender ?? "none",
+        memberBirthday: data.memberBirthday ?? null,
+      });
+      setEmailDomain(emailLast);
+    };
+
+    getInfo();
+  },[member?.memberId, reset]);
 
   const nameRegister = register("memberName", {
     required: "이름을 입력해주세요.",
@@ -62,12 +92,74 @@ export default function MyUpdatePage() {
     }
   };
 
+  const handleUpdate = async(data:JoinFormDTO) => {
+
+    // 이메일 단어 합쳐서 서버로 전달
+		const email = data.emailFirst && data.emailLast ? `${data.emailFirst}@${data.emailLast}` : "";
+
+    // "" 값 방지용, 선택사항을 선택하지 않은 경우엔 null 전송
+		const requestData = {
+			memberName: data.memberName,
+			memberEmail: email,
+			memberPhone : data.memberPhone?.trim() ? data.memberPhone.trim() : null,
+			memberGender : data.memberGender || "none",
+			memberBirthday : data.memberBirthday || null,
+		}
+
+    // 에러방지 코드
+    if (!member?.memberId) return;
+
+    try {
+			await memberApi.update(member?.memberId, requestData);
+			// 정보 수정 성공
+			console.log(requestData);
+      alert("정보 수정 성공!");
+
+    } catch(error) {
+			if (!axios.isAxiosError(error)) {
+				console.error(error);
+				return;
+			}
+			const errorName = error.response?.data?.name;
+			const errorMessage = error.response?.data?.message;
+
+			// 상황별 errorCode 정리
+			if (errorName==="MEMBER_ID_EXIST") {
+				setError("memberId", {
+						type: "server",
+						message: errorMessage
+					});
+			} else if (errorName==="MEMBER_EMAIL_EXIST") {
+				setError("emailFirst", {
+						type: "server",
+						message: errorMessage
+					});
+			}
+			alert("정보 수정 실패");
+		}
+  };
+
   return (
+    <form onSubmit={handleSubmit(handleUpdate)}>
     <section className={s.page}>
       <header className={s.header}>
         <h3>내 정보 수정</h3>
         <p>아이디와 비밀번호를 제외한 회원 정보를 수정할 수 있습니다.</p>
       </header>
+
+      <div className={s.formRow}>
+        <label htmlFor="memberId">아이디</label>
+        <div className={s.disableBox}>
+          <input id="memberId" type="text" {...register("memberId")} disabled />
+        </div>
+      </div>
+
+      <div className={s.formRow}>
+        <label htmlFor="memberPw">비밀번호</label>
+        <div className={s.disableBox}>
+          <input id="memberPw" type="password" value="********" disabled />
+        </div>
+      </div>
 
       <div className={s.formBox}>
         <section className={s.formRow}>
@@ -135,5 +227,6 @@ export default function MyUpdatePage() {
 
       <button type="submit" className={s.btn}>정보 수정</button>
     </section>
+    </form>
   );
 }
